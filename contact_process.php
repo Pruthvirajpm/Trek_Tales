f7618e6a5d7636c046aeb7acec247eb28bb23784
Chioma Onyekaba <133171635+ChiomaOnyekaba55@users.noreply.github.com>
2023-11-01 19:07:36 -0400

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $message = $_POST["message"];
 
-    // Validate and sanitize user input
+Validate and sanitize user input
 
     $sql = "INSERT INTO contact_messages (message_id, name, email, phone, title, message) VALUES ('$msgid','$name', '$email', '$phone','$title','$message')";
 
